09a40089039d6d532fc5dd5dfb20dade9db8a8fc
Clarissa Paz <clarissp@andrew.cmu.edu>
2018-10-19 15:32:18 -0400

    Errors everywhere
    There are erros on all of my pages and I don't know why

--- app.R
+++ app.R
@@ -21,23 +21,25 @@ pacounty <- readOGR("PA_Counties_clip.shp")
 #Subsetting counties to Southwest counties 
 swcounty <- c("Armstrong", "Allegheny", "Beaver", "Cambria", "Fayette", "Greene", "Indiana", "Somerset", "Washington", "Westmoreland")
 pa_swcounty <- pacounty[pacounty$NAME %in% swcounty,]
+
+#Transofrming projection of counties to match the following two layers 
 proj4string(pa_swcounty) <- CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
 pa_swcounty <- spTransform(pa_swcounty, CRS=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
-
 #API for the Permit data 
 permits <- readOGR("http://data-padep-1.opendata.arcgis.com/datasets/cea4b401782a4178b139c6b5c6a929f2_48.geojson")
+permitcounty <- c("Armstrong", "Beaver", "Cambria", "Greene", "Indiana", "Somerset", "Washington", "Westmoreland")
+sw_permits <- permits[permits$COUNTY %in% permitcounty,]
+
+#CSV for Permit data 
+permitdata <- read.csv("Active_Underground_Permit_Boundaries.csv") 
+sw_permitdata <- filter(permitdata, COUNTY == "Armstrong" | COUNTY == "Beaver" | COUNTY == "Cambria" | COUNTY == "Greene" | COUNTY ==  "Indiana" | COUNTY == "Somerset" | COUNTY ==  "Washington" | COUNTY == "Westmoreland")
 
 #API for Environmental Good Samaritan Act points 
 goodact <- readOGR("http://data-padep-1.opendata.arcgis.com/datasets/f5487b2bd296492097994a8ab5bd4c9b_261.geojson")
 
 #Creating county column for Environmental Good Samaritan Act points (goodact)
 goodact$county <- over(goodact, pa_swcounty, fn = NULL)
-
-#Reading in Abandoned Mine Lands data from DEP 
-#aml <- read.csv("Abandoned_Mine_Land_Inventory_Polygons.csv") 
-#amlsubset <- subset(aml, select = c("SF_TYPE", "SF_STATUS_CD", "SF_STATUS", "SF_PRIORITY", "SF_PROBLEM_CODE", "HEIGHT_FT", "VOLUME_CY"))
-#amlsubset
 
 #Header of the shiny dashboard 
 header <- dashboardHeader(title = "Siting Grid-Scale Solar in Pennsylvania")
@@ -47,26 +49,18 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     id = "tabs",
     #Pages in the sidebar 
-    menuItem("Active Underground Permits", icon = icon("globe"), tabName = "mines"),
-    menuItem("Dataset", icon = icon("database"),tabName = "minetable"),
+    menuItem("Active Underground Permits", icon = icon("globe"), tabName = "activepermit"),
+    menuItem("Dataset", icon = icon("database"),tabName = "permittable"),
     menuItem("Map", tabName = "permit"),
     
-    #Select input for Type of AMLs
-    selectInput("amltype",
-                "Abandoned Mine Land Type(s):",
-                choices = sort(unique(aml$SF_TYPE)),
+    #Select input for Type of Permits
+    selectInput("permittype",
+                "Permit Type(s):",
+                choices = sort(unique(sw_permitdata$TYPE)),
                 multiple = TRUE,
-                selected = c("Coal Deep Mine", "Coal Surface Mine")
+                selected = c("Room and Pillar")
                 ),
     
-    #Slider input for Height of AMLS 
-    sliderInput("height",
-                "Height of Abandoned Mine Lands:",
-                min = min(aml$HEIGHT_FT, na.rm = T),
-                max = max(aml$HEIGHT_FT, na.rm = T),
-                value = c(min(aml$HEIGHT_FT, na.rm = T),
-                          max(aml$HEIGHT_FT, na.rm = T)),
-                step = 50),
     #Select input for Counties in Permit Map 
     selectInput("counties",
                 "Select a County:",
@@ -82,21 +76,20 @@ sidebar <- dashboardSidebar(
 #Body of the shiny dashboard 
 body <- dashboardBody(
   tabItems(
-    tabItem("nature"),
-    tabItem("mines",
+    tabItem("activepermit",
             fluidRow(
               width = 12,
-              tabPanel("Bar Plot", plotlyOutput("amlbar"))
+              tabPanel("Bar Plot", plotlyOutput("permitbar"))
               )
             ),
-    tabItem("minetable",
+    tabItem("permittable",
             fluidPage(
-              box(title = "Abandoned Mine Land Dataset", DT::dataTableOutput("amltable"), width = 12)
+              box(title = "Abandoned Mine Land Dataset", DT::dataTableOutput("permittable"), width = 12)
               )
             ),
     tabItem("permit",
             fluidPage(
-              leafletOutput("permitmap")
+              box(title = "Active Permits in Southwest PA", leafletOutput("permitmap"), width = 12)
             ))
     )
   )
@@ -107,11 +100,10 @@ ui <- dashboardPage(header, sidebar, body, skin = "black")
 server <- function(input, output, session = session){
   #Reactive function for all pages (global inputs)
   globalInput <- reactive({
-    global <- aml %>%
-      filter(HEIGHT_FT >= input$height[1] & HEIGHT_FT <= input$height[2])
+    global <- sw_permitdata %>%
              
-      if (length(input$amltype) > 0 ) {
-        global <- subset(aml, SF_TYPE %in% input$amltype)
+      if (length(input$permittype) > 0 ) {
+        global <- subset(sw_permitdata, TYPE %in% input$permittype)
       }
     
     if (length(input$counties) > 0 ) {
@@ -140,13 +132,21 @@ server <- function(input, output, session = session){
       ) 
   })
   
-  output$amlbar <- renderPlotly({
+  output$permitpie <- renderPlotly({
+    plot_ly(data = data.frame(sw_permitdata), labels = permits$COUNTY, type = 'pie',
+            textposition = 'inside',
+            textinfo = 'label+percent', insidetextfont = list(color = '#FFFFFF'),
+            hoverinfo = 'label+percent', showlegend = TRUE) 
+    
+  })
+  
+  output$permitbar <- renderPlotly({
     global <- globalInput()
-    ggplot(data = aml, aes(x = SF_PRIORITY, fill = SF_STATUS)) +
+    ggplot(data = sw_permitdata, aes(x = COUNTY, fill = STATUS)) +
       geom_bar(stat = "count") +
-      labs(title = "Abandoned Mine Lands (AML) in Pennsylvania", 
-           x= "Priority of AML", 
-           y= "Count of AMLs", fill = "Status"
+      labs(title = "Active Underground Permits in Pennsylvania", 
+           x= "County", 
+           y= "Count of Permits", fill = "Status"
       ) +
       scale_fill_brewer(palette = "Pastel1") +
       theme_bw() +
@@ -180,19 +180,16 @@ server <- function(input, output, session = session){
     filter <- ifelse(length(input$counties) > 0, 
                            paste0("COUNTY+IN%+(%27", paste(input$counties, collapse = "%27,%27"),"%27)"),
                            "")
-    #url <- paste0("http://www.depgis.state.pa.us/arcgis/rest/services/emappa/eMapPA_External_Extraction/MapServer/48/query?where=", filter, "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson")
-    
     url <- paste0("http://www.depgis.state.pa.us/arcgis/rest/services/emappa/eMapPA_External_Extraction/MapServer/48/query?where=COUNTY+IN+%28%27Armstrong%27%2C+%27Beaver%27%2C+%27Cambria%27%2C+%27Greene%27%2C+%27Indiana%27%2C+%27Somerset%27%2C+%27Washington%27%2C+%27Westmoreland%27%29",filter, "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelWithin&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson")
     permits <- readOGR(url)
   })
   
-  output$amltable <- DT::renderDataTable({
-    subset(globalInput(), select = c("SF_TYPE", "SF_STATUS_CD", "SF_STATUS", "SF_PRIORITY", "SF_PROBLEM_CODE", "HEIGHT_FT", "VOLUME_CY"))
+  output$permittable <- DT::renderDataTable({
+    subset(globalInput(), select = c("MINE", "OPERATOR", "TYPE", "STATUS", "COAL_SEAM", "COUNTY"))
     })
   
   observeEvent(input$reset, {
-    updateSelectInput(session, "amltype", selected = c("Coal Deep Mine", "Coal Surface Mine"))
-    updateSliderInput(session, "height", value = c(min(aml$HEIGHT_FT, na.rm = T), max(aml$HEIGHT_FT, na.rm = T)))
+    updateSelectInput(session, "permittype", selected = c("Room and Pillar"))
     updateSelectInput(session, "counties", selected = c("Somerset"))
     showNotification("You have successfully reset the filters", type = "message")
   })
